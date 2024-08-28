7fe3121a02bb69cb00d8765f80460794d5bea492
Brady <bdullanty@ewu.edu>
2024-08-27 18:17:43 -0700

    Initial setup for front end

---
+++ FrontEnd/reactfirst/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <p>
+          Click Below to update a number in the database, and change it based on the new result.
+        </p>
+        <a
+          className="App-link"
+          href="https://ohhell.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=44i7gporhs72lcdgs0ht1ak564&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fbdullanty.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
