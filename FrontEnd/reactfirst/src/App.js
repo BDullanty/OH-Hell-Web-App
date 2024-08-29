a22f5f4a7e93a5abffd08f361061fe5cbabb4162
Brady <bdullanty@ewu.edu>
2024-08-29 16:35:16 -0700

    Removed manual pkce implementation and added in more logs and checks

--- FrontEnd/reactfirst/src/App.js
+++ FrontEnd/reactfirst/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {OAuthHandler} from './Components/OAuthHandler';
--- FrontEnd/reactfirst/src/Components/OAuthHandler.js
+++ FrontEnd/reactfirst/src/Components/OAuthHandler.js
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react';
 import axios from 'axios';
-generateCodeVerifier();
-generateCodeChallenge();
+import pkceChallenge from 'pkce-challenge';
+import { verifyChallenge } from "pkce-challenge";
+ 
 
-  export const createAuthLink = async()=> {
+  //creates the initial request for the token.
+  //We are using PKCE so we must generate a challenge and send it
+  export const createAuthLink = () =>{
     console.log('in createAuthLink')
     console.log('Verifier initially:'+localStorage.getItem('verifier'))
     const codeChallenge = localStorage.getItem('challenge');
     const clientId = '44i7gporhs72lcdgs0ht1ak564';
-    const redirectUri = encodeURIComponent('http://localhost:3000/oauth/callback');
+    const redirectUri = encodeURIComponent('https://main.dmqlib7blr1by.amplifyapp.com/');
     const scopes = encodeURIComponent('email openid phone');
     const codeChallengeMethod = 'S256';
     console.log()
@@ -30,25 +33,28 @@ export const OAuthHandler = () => {
     const handleOAuthRedirect = async () => {
       const urlParams = new URLSearchParams(window.location.search);
       const authCode = urlParams.get('code');
-      const redirectUri = encodeURIComponent('http://localhost:3000/oauth/callback'); // Should match the one used in the initial request
+      const redirectUri = 'https://main.dmqlib7blr1by.amplifyapp.com/oauth/callback';
 
       if (authCode) {
         console.log('AuthCode:', authCode);
-        const codeVerifier = localStorage.getItem('code_verifier'); // Retrieve the codeVerifier
+        const codeVerifier = localStorage.getItem('verifier'); // Retrieve the codeVerifier
         console.log('OAuth Verifier:', codeVerifier);
         
         if (!codeVerifier) {
           console.error('Code verifier not found.');
           return;
         }
-        const OAuthURL = `https://ohhell.auth.us-east-1.amazoncognito.com/oauth2/token`;
-        const data = {
+        const OAuthURL = `https://ohhell.auth.us-east-1.amazoncognito.com/oauth2/token?`;
+        console.log("Verifier for token request:",codeVerifier);
+        checkIfValid();
+        const data = new URLSearchParams({
           client_id: '44i7gporhs72lcdgs0ht1ak564',
           grant_type: 'authorization_code',
-          code: `${authCode}`,
-          redirect_uri: encodeURIComponent(`${redirectUri}`),
-          code_verifier: `${codeVerifier}`
-        };
+          code: authCode,
+          code_verifier: codeVerifier,
+          redirect_uri: redirectUri,
+
+        });
         console.log(OAuthURL)
         console.log(data)
         try {
@@ -57,9 +63,11 @@ export const OAuthHandler = () => {
             data,
             {
               headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
+                'Content-Type': 'application/x-www-form-urlencoded',
               }
-            }
+            },
+            
+
           );
 
           const { access_token, refresh_token } = response.data;
@@ -75,40 +83,21 @@ export const OAuthHandler = () => {
   }, []);
   return <div>Handling OAuth...</div>;
 };
-function dec2hex(dec) {
-  return ("0" + dec.toString(16)).substr(-2);
-}
-
-function generateCodeVerifier() {
-  var array = new Uint32Array(56 / 2);
-  window.crypto.getRandomValues(array);
-  
-  localStorage.setItem('verifier',Array.from(array, dec2hex).join(""));
-  console.log('Logged verifier',localStorage.getItem('verifier'));
- 
-}
-function sha256(plain) {
-  // returns promise ArrayBuffer
-  const encoder = new TextEncoder();
-  const data = encoder.encode(plain);
-  return window.crypto.subtle.digest("SHA-256", data);
-}
-
-function base64urlencode(a) {
-  var str = "";
-  var bytes = new Uint8Array(a);
-  var len = bytes.byteLength;
-  for (var i = 0; i < len; i++) {
-    str += String.fromCharCode(bytes[i]);
+export async function checkIfValid(){
+  try{
+    const result = await verifyChallenge(localStorage.getItem('verifier'), localStorage.getItem('challenge')) ===
+  true // true){
+    console.log("Challenge is valid. ",result)
+  } catch(error){
+    console.log('Challenge FAILED');
   }
-  return btoa(str)
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=+$/, "");
 }
-
-async function generateCodeChallenge(v) {
-  var hashed = await sha256(v);
-  var base64encoded = base64urlencode(hashed);
-  localStorage.setItem("challenge",base64encoded);
+export async function generateChallenge(){
+  
+  const response = await pkceChallenge();
+  console.log('Generating Challenge',response);
+  localStorage.setItem('verifier',response.code_verifier);
+  localStorage.setItem('challenge',response.code_challenge);
+  console.log('Logged verifier',localStorage.getItem('verifier'));
+  createAuthLink();
 }
