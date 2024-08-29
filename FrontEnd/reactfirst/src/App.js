0b56e9aa89c0a75e47e909f5c6001f7df0b08e29
Brady <bdullanty@ewu.edu>
2024-08-28 22:22:52 -0700

    set app page to be routes

--- FrontEnd/reactfirst/src/App.js
+++ FrontEnd/reactfirst/src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {OAuthHandler} from './Components/OAuthHandler';
+import HomePage from './HomePage';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Click Below to update a number in the database, and change it based on the new result.
-        </p>
-        <a
-          className="App-link"
-          href="https://ohhell.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=44i7gporhs72lcdgs0ht1ak564&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fmain.dmqlib7blr1by.amplifyapp.com"
-          target=""
-          rel="noopener noreferrer"
-        >
-          Log In
-        </a>
-      </header>
-    </div>
+
+      <Router>
+      <Routes>
+        <Route path="/oauth/callback/" Component={OAuthHandler} />
+        {/* Other routes */}
+        <Route path="/" Component={HomePage}>
+         
+        </Route>
+      </Routes>
+    </Router>
   );
 }
-
 export default App;
