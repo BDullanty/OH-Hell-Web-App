5a200818899afcb077fc4c10fcbf33a820e897ed
Brady <bdullanty@ewu.edu>
2024-08-30 14:03:21 -0700

    Added counter to app.js

--- FrontEnd/reactfirst/src/App.js
+++ FrontEnd/reactfirst/src/App.js
@@ -4,15 +4,17 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import {OAuthHandler} from './Components/OAuthHandler';
 import HomePage from './Pages/HomePage';
 import NotFound from './Pages/404Page';
+import Counter from './Pages/Counter';
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/oauth/callback" element={<OAuthHandler/>} />
         <Route path="/oauth/callback/" element={<OAuthHandler/>} />
+        
+        <Route path="/counter" element={<Counter/>} />
         <Route path="/" element={<HomePage/>}/>
         <Route path="*" element={<NotFound/>} />
-       
       </Routes>
     </BrowserRouter>
   );
