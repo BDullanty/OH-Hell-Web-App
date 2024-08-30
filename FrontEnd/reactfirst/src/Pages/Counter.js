040f9294b3a483fa72665b4174f1007ca49f9a5e
Brady <bdullanty@ewu.edu>
2024-08-30 14:03:11 -0700

    Added counter page for successful logins

---
+++ FrontEnd/reactfirst/src/Pages/Counter.js
@@ -0,0 +1,26 @@
+
+import {Button} from '@mui/material';
+const Counter = () =>{
+ 
+  return (
+    <div className="App">    
+
+    <header className="App-header">
+  <p>
+  Click Below change the database number! (not yet)
+  </p>
+ 
+  <Button
+  onClick={() => {
+  }}
+>
+  Click me
+</Button>
+    </header>
+  </div>
+
+  );
+  
+}
+
+export default Counter;
