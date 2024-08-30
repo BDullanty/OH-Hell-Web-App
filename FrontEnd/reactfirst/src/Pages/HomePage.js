47fdebd1317219faae5297939698c6f578f17eed
Brady <bdullanty@ewu.edu>
2024-08-30 14:03:43 -0700

    Fixed challenge being a promise

--- FrontEnd/reactfirst/src/Pages/HomePage.js
+++ FrontEnd/reactfirst/src/Pages/HomePage.js
@@ -1,10 +1,26 @@
 
 import '../App.css';
+import React, { useState, useEffect } from 'react';
 import {generateChallenge} from '../Components/OAuthHandler';
+import {Button} from '@mui/material';
 const HomePage = () =>{
-  generateChallenge();
-  console.log('IN HomePage')
-  console.log('AuthURL:'+localStorage.getItem('authUrl'))
+  
+  const [authUrl, setAuthUrl] = useState(''); // Initialize state with empty string
+
+  useEffect(() => {
+    const fetchAuthUrl = async () => {
+      try {
+        const url = await generateChallenge(128); // Await the async function
+        setAuthUrl(url); // Update state with the resolved URL
+      } catch (error) {
+        console.error('Error generating challenge:', error);
+      }
+    };
+
+    fetchAuthUrl(); // Call the async function when component mounts
+  }, []); // Empty dependency array to run only once on mount
+
+
   return (
     <div className="App">    
 
@@ -12,14 +28,14 @@ const HomePage = () =>{
   <p>
   Click Below to Sign In.
   </p>
-  <a
-  className="App-link"
-  href={localStorage.getItem('authUrl')}
-  target=""
-  rel="noopener noreferrer"
-  >
-  Login test
-  </a>
+
+  <Button
+  onClick={() => {
+    window.location.replace(authUrl);
+  }}
+>
+  Login
+</Button>
     </header>
   </div>
 
