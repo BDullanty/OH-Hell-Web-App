f2e6bc114adf4cd34a67f822608fb11e6d5a799f
Brady <bdullanty@ewu.edu>
2024-09-10 20:51:51 -0700

    Added in Connect Handler

--- Backend/OHHellHTTPServer.java
+++ Backend/OHHellHTTPServer.java
@@ -10,12 +10,29 @@ public class GameHttpServer {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         //defines all route paths into their methods.
+        server.createContext("/Connect", new ConnectHandler());
         server.createContext("/PlayCard", new PlayCardHandler());
         server.createContext("/ListPlayers", new ListPlayers());
         server.setExecutor(null); // Creates a default executor
         server.start();
         System.out.println("HTTP server started on port 8080");
     }
+    //Handles intial connections
+    //We want to add this player to the online list
+    static class ConnectHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Player Connected";
+            String requestBody = new String(exchange.getRequestBody().readAllBytes());
+            System.out.println("Player Name: " + requestBody);
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+
+            os.close();
+        }
+    }
+
     //Handles all /PlayCard calls.
     static class PlayCardHandler implements HttpHandler {
         @Override
