191659607891ce50e232600ef00afe7571726bde
Brady <bdullanty@ewu.edu>
2024-09-12 13:04:16 -0700

    Implemented a Player Object
    Contains static hashmaps of all players

--- Backend/ec2 HTTP server/Player.java
+++ Backend/ec2 HTTP server/Player.java
@@ -1,26 +1,50 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Player {
-    String sub;
-    String name;
-    public Player(String sub){
-        this.name = resolveNameFromSub(sub);
+    //Static Values
+    //TODO:Change the new Arraylist to load methods that load from Database.
+    private static HashMap<String,Player> playerList = new HashMap();
+    private static ArrayList<Player> onlineList = new ArrayList<>();
+
+    //Per Player Values
+    private String sub;
+    private String username;
+
+
+    public String getSub() {
+        return sub;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Player(String sub,String username){
+        this.username = username;
         this.sub=sub;
     }
 
-    private String resolveNameFromSub(String sub) {
-        return "Name from backend";
-    }
-    public static Player getPlayer(String sub){
-        //if they are online, send the existing object
-        Player alreadyOnline =returnPlayerIfOnline(sub);
-        if(alreadyOnline !=null) return alreadyOnline;
-        //otherwise create
-        Player newPlayer = new Player(sub);
+    //STATIC FUNCTIONS
+
+    //Returns a player object unless one is already made
+    public static Player getPlayer(String sub,String username){
+        //if the player object exists, send it.
+        System.out.println("Finding player with username "+username);
+        if(Player.playerList.containsKey(sub)){
+        return playerList.get(sub);
+        }
+        //If not, we create the object
+        Player newPlayer = new Player(sub,username);
+        playerList.put(newPlayer.getSub(),newPlayer);
         return newPlayer;
     }
-
-    private static Player returnPlayerIfOnline(String sub) {
-        //check if sub is in player list;
-        return new Player(sub);
+    public static void addPlayerOnline(Player p){
+        if(p==null) throw new IllegalArgumentException("Player p was null");
+     Player.onlineList.add(p);
+    }
+    public static void removeOnlinePlayerOnline(Player p){
+        Player.onlineList.remove(p);
     }
 
 }
