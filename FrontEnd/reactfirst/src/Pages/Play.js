2e83c7f91956b7c88905e894d8c94b79479b1e8e
Brady <bdullanty@ewu.edu>
2024-09-12 16:47:18 -0700

    Moved click logic into functions

--- FrontEnd/reactfirst/src/Pages/Play.js
+++ FrontEnd/reactfirst/src/Pages/Play.js
@@ -6,16 +6,11 @@ import { getCookie } from '../Components/cookies';
 let LobbyListOnCooldown= false;
 const cooldownTime = 5000;//1000 is one second
 
-
+let connectMessage;
+let disconnectMessage;
 const Play = () =>{
   connectWebSocket();
-  const connectMessage = 
-
-JSON.stringify({
-  action: "connect",
-  jwk: `${getCookie('access_token')}`  
-});
-
+ 
   return (
     <div className="App">    
 
@@ -25,24 +20,60 @@ JSON.stringify({
   <Button
   onClick={() => {
 
-   
-    if(!LobbyListOnCooldown){
-      console.log("Request Json: ",connectMessage);
-      socket.send(connectMessage);
-      LobbyListOnCooldown=true;
-      setTimeout(()=>{
-          LobbyListOnCooldown=false
-        }, cooldownTime);
-      }
+    connectOnline();
     
   }}
 >
   Connect to backend
+</Button>
+<Button
+  onClick={() => {
+  disconnect();
+
+    
+  }}
+>
+  Disconnect from backend
 </Button>
     </header>
   </div>
 
   );
   
+}
+
+const connectOnline = () => {  
+  connectMessage = 
+  JSON.stringify({
+    action: "connect",
+    jwk: `${getCookie('access_token')}`  
+  });
+
+  if(!LobbyListOnCooldown){
+  console.log("Request Json: ",connectMessage);
+  socket.send(connectMessage);
+  LobbyListOnCooldown=true;
+  setTimeout(()=>{
+      LobbyListOnCooldown=false
+    }, cooldownTime);
+  }
+  //We can set the disconnect message now
+
+}
+const disconnect = () =>{
+  disconnectMessage = 
+  JSON.stringify({
+    action: "disconnect",
+    sub: `${getCookie('sub')}`  
+  });
+  
+  if(!LobbyListOnCooldown){
+    console.log("Disconnected!");
+    socket.close(200,disconnectMessage);
+    LobbyListOnCooldown=true;
+    setTimeout(()=>{
+        LobbyListOnCooldown=false
+      }, cooldownTime);
+    }
 }
 export default Play;
