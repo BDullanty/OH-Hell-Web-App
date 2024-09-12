3d97219619052b29044a5316de84af2eb7cd0a8f
Brady <bdullanty@ewu.edu>
2024-09-12 00:55:03 -0700

    Created Json Handler from import

--- Backend/ec2 HTTP server/JsonHandler.java
+++ Backend/ec2 HTTP server/JsonHandler.java
@@ -1,6 +1,25 @@
+import org.json.JSONObject;
 public class JsonHandler {
-    public static String getSubFromBody(String body){
-        return "1";
+    //Expected String Input:
+    //{"action":"something","sub":"something", ....}
+    public static String getJWKFromBody(String body){
 
+        try {
+            // Parse the JSON string
+            JSONObject jsonObject = new JSONObject(body);
+
+            // Extract the value associated with the "sub" key
+            String token= jsonObject.getString("token");
+
+            System.out.println("Token: "+token);  // Output: something
+            return token;
+        }
+        catch (Exception e){
+            throw new IllegalArgumentException("Passed in string was not converted properly");
+        }
+    }
+    public static String getNameFromJWK(String jwk) {
+        String username = "";
+        return username;
     }
 }
