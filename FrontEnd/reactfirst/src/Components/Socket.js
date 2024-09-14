4881c5df9fdbcb5c81655370fc65fb2d1c6e8ca5
Brady <bdullanty@ewu.edu>
2024-09-13 23:38:05 -0700

    More console.log changes

--- FrontEnd/reactfirst/src/Components/Socket.js
+++ FrontEnd/reactfirst/src/Components/Socket.js
@@ -1,45 +1,30 @@
-import {getCookie, setCookie} from './cookies';
-
-
+import {getCookie} from './cookies';
 
 export let socket = null;
 export async function connectWebSocket() {
   const token = getCookie('access_token');
   try {
   const url = "wss://sbokdz62pc.execute-api.us-west-1.amazonaws.com/production?Authorization="+token;
-  
 
     socket = new WebSocket(url);
 
     socket.addEventListener('open', function (event) {
-      console.log('WebSocket connection opened :)');
-      console.log('On open we got: ',event);
-      console.log('On open we got: ',event);
+      console.log('Connection Established');
     });
 
     socket.addEventListener('message', function (event) {
       console.log("Raw Response: ",event.data);
       const jsonData = JSON.parse(event.data);
-
+      //TODO: change below to switch case for returnType parsing.
       if(jsonData.returnType === 'error'){
-      console.log("Bad Request",jsonData.error)
-      }
-      else if(jsonData.returnType === 'connect'){
-      console.log("connect response:",jsonData)
-      setCookie("sub",jsonData.sub,1);
-      setCookie("username",jsonData.username,1);
-      console.log("Sub logged as: ", getCookie("sub"));
-      console.log("username logged as: ", getCookie("username"));
+      console.log("ReturnType: error, message: ",jsonData.error)
       }
       else{
-      console.log('Message from server:', jsonData);
       }
     });
 
     socket.addEventListener('close', function (event) {
       console.log('WebSocket connection closed');
-      console.log('Message from server on close: ',event.reason);
-      console.log('Message from server on close: ',event);
 
     });
 
