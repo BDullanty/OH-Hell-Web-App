a6455f1fd7b17e6b64dd1852dcf0cc53ce09f19c
Brady <bdullanty@ewu.edu>
2024-09-19 14:49:49 -0700

    Refactored Game class

--- Backend/ec2 HTTP server/GameHandlers/GameHandler.java
+++ Backend/ec2 HTTP server/GameHandlers/GameHandler.java
@@ -22,29 +22,41 @@ public class GameHandler {
     }
 
     public static void start(Game game){
-
-        game.setState(State.INGAME);
         //to start game, we will set the state to in game
+        game.setState(State.INGAME);
+        lobbyGames.remove(game.getGameID());
+        liveGames.put(game.getGameID(), game);
+        //TODO:
+        //We must add in bot players for remaining slots
+        //We will loop n rounds, each round players get cards, bet, and do sub rounds up to n
+        //We have subrounds where players play cards
+        //Host will lead the first bet round.
+        //each sub round will:
+        // Have a player play a card.
+        //After each sub round, we will clean up, and do next round.
+
+
+        //Each game must track:
+        //Whos turn it is
+        //What bets were taken
+        //What hands they have
+        //
+
     }
-    public static void bet(Player p, int bet){
+    public static void bet(Player p, int bet,Game game){
+
 
     }
     public static void addGameToLobby(Game game){
 
         lobbyGames.put(game.getGameID(),game);
     }
-    public static void moveGameToLive(Game game){
-        lobbyGames.remove(game.getGameID());
-        liveGames.put(game.getGameID(),game);
-    }
+    public static void end(Game game){
 
-    public static void moveGameToFinished(Game game){
+        game.setState(State.FINISHED);
+        //to start game, we will set the state to in game
         liveGames.remove(game.getGameID());
         finishedGames.put(game.getGameID(),game);
     }
-    public static void end(Game game){
 
-        game.setState(State.INGAME);
-        //to start game, we will set the state to in game
-    }
 }
