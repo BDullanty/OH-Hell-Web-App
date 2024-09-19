cf9c3ca9f229c37fc39115b7c2a0b3b2de8bb58e
Brady <bdullanty@ewu.edu>
2024-09-19 14:25:00 -0700

    Made Player abstract so we can extend to User and Bot

--- Backend/ec2 HTTP server/GameHandlers/Player.java
+++ Backend/ec2 HTTP server/GameHandlers/Player.java
@@ -2,71 +2,14 @@ package GameHandlers;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+//Can be a bot or a user
+public abstract class Player{
 
-public class Player {
-    //Static Values
-    //TODO:Change the new Arraylist to load methods that load from Database.
-    private static HashMap<String,Player> playerList = new HashMap<>();
-    private static HashMap<String,Player> connectionList = new HashMap<>();
-    private static ArrayList<Player> onlineList = new ArrayList<>();
 
-    //Per GameHandler.Player Values
-    private String sub;
-    private String username;
-    private String connectionID;
+    protected String username;
 
-    public static Player getPlayer(String connectionID) {
-        return connectionList.get(connectionID);
-    }
 
-    private Player setConnection(String connectionID) {
-
-        System.out.println("ConnectionID changed from " + this.connectionID+" to "+connectionID);
-        connectionList.put(connectionID,this);
-        this.connectionID = connectionID;
-        return this;
-    }
-    public String getSub() {
-        return sub;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public Player(String sub,String username, String connectionID){
+    public Player(String username) {
         this.username = username;
-        this.sub=sub;
-        this.connectionID = connectionID;
-        connectionList.put(connectionID,this);
     }
-
-    //STATIC FUNCTIONS
-
-    //Returns a player object unless one is already made
-    public static Player getPlayer(String sub,String username, String connectionID){
-        //if the player object exists, send it.
-        System.out.println("Finding player with username "+username);
-        if(Player.playerList.containsKey(sub)){
-            return playerList.get(sub).setConnection(connectionID);
-        }
-        //If not, we create the object
-        Player newPlayer = new Player(sub,username,connectionID);
-        playerList.put(newPlayer.getSub(),newPlayer);
-        return newPlayer;
-    }
-
-
-    public static void addPlayerOnline(Player p){
-        if(p==null) throw new IllegalArgumentException("Player was null");
-        if(onlineList.contains(p)) System.out.println("Player was already online? This is a bug.");
-        Player.onlineList.add(p);
-    }
-    public static Player removeOnlinePlayer(String connectionID){
-        Player offlinePlayer = Player.connectionList.get(connectionID);
-        System.out.println("Player " +offlinePlayer.getUsername()+ " has gone offline.");
-        Player.onlineList.remove(offlinePlayer);
-        return offlinePlayer;
-    }
-
 }
