5cc7466c1254eb953a585bfb5ba4f0eaad557ac5
Brady <bdullanty@ewu.edu>
2024-09-21 14:39:18 -0700

    Added a postAllUsersToLobby class

---
+++ Backend/ec2 HTTP server/HTTPHandlers/PostAllUsersToLobby.java
@@ -0,0 +1,23 @@
+package HTTPHandlers;
+
+import GameHandlers.User;
+
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+
+public class PostAllUsersToLobby {
+    
+    public static void postAllUsersToLobby(){
+        ArrayList<String> lobbyConnections = User.getLobbyConnections();
+        String message = "{\"returnType\": \"playerList\", \"users\" : "+ User.getUsers();
+        try {
+            AWSSigner.sendSignedMessage(message,lobbyConnections);
+            System.out.println("Sent "+message+" to lobby players");
+        } catch (Exception e) {
+            System.out.println("Failed to send message : "+e);
+
+        }
+    }
+    
+}
