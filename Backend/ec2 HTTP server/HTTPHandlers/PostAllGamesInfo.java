1d345ebe5b7c9be56677363c6edab961f8b7f92a
Brady <bdullanty@ewu.edu>
2024-09-22 13:29:00 -0700

    Changed file name to be more accurate. Added new method

--- Backend/ec2 HTTP server/HTTPHandlers/PostAllGamesInfo.java
+++ Backend/ec2 HTTP server/HTTPHandlers/PostAllGamesInfo.java
@@ -1,16 +1,29 @@
 package HTTPHandlers;
 
-import GameHandlers.Game;
 import GameHandlers.GameHandler;
-import GameHandlers.Player;
 import GameHandlers.User;
 
 import java.util.ArrayList;
 
-public class PostAllGamesToLobby {
+public class PostAllGamesInfo {
     public static void postAllGamesToLobby() {
         try {
             ArrayList<String> lobbyConnections = User.getLobbyConnections();
+            String message = "{\"returnType\": \"gameList\", \"games\" : " + GameHandler.getLobbyGamesJson() + "}";
+
+            AWSSigner.sendSignedMessage(message, lobbyConnections);
+            System.out.println("Sent " + message + " to lobby players");
+        } catch (Exception e) {
+            System.out.println("Failed to postAllGamesToLobby : " + e);
+
+        }
+
+
+    }
+
+    public static void postAllGamesToUser(User user) {
+        try {
+            ArrayList<String> lobbyConnections = user.getConnections();
             String message = "{\"returnType\": \"gameList\", \"games\" : " + GameHandler.getLobbyGamesJson() + "}";
 
             AWSSigner.sendSignedMessage(message, lobbyConnections);
