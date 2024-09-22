11fe5fcbd5f8a565380d06f575082e5bc767a7ad
Brady <bdullanty@ewu.edu>
2024-09-22 13:25:23 -0700

    Moved hand, bet, and gameID up to Player object

--- Backend/ec2 HTTP server/GameHandlers/Player.java
+++ Backend/ec2 HTTP server/GameHandlers/Player.java
@@ -7,9 +7,43 @@ public abstract class Player{
 
 
     protected String username;
-
+    protected int gameID;
+    protected ArrayList<Card> hand;
 
     public Player(String username) {
         this.username = username;
+        this.gameID = -1;
+        this.hand = new ArrayList<>();
     }
+
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(int gameID) {
+        gameID = gameID;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    public int getBet() {
+        return bet;
+    }
+
+    public void setBet(int bet) {
+        this.bet = bet;
+    }
+
+    protected int bet;
+
+
+
+
 }
