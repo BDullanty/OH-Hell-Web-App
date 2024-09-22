2033aa8d8f62519695a80589988db225b2b08ab9
Brady <bdullanty@ewu.edu>
2024-09-21 19:56:06 -0700

    Added in a lobby list generator

---
+++ FrontEnd/reactfirst/src/Components/lobbylist.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Paper, List, ListItem, ListItemText, Typography } from '@mui/material';
+export const Lobbylist = ({ lobbyList }) => {
+  {
+
+    return (
+      <div>
+        <Paper elevation={2} sx={{}}>
+          <Typography variant="h6">Lobby List:</Typography>
+          <List>
+            {Object.entries(lobbyList).map(([gameID, gameDetails]) => (
+              <ListItem key={gameID}>
+                <ListItemText primary={`${gameDetails.host}'s game`} secondary={gameDetails.status} />
+                
+                <List>
+                  {[gameDetails.host, gameDetails.player2, gameDetails.player3, gameDetails.player4, gameDetails.player5].map((player, index) => (
+                    player && (
+                      <ListItem key={index}>
+                        <ListItemText primary={player} />
+                      </ListItem>
+                    )
+                  ))}
+                </List>
+
+                {gameDetails.status === "INGAME" && (
+                <Typography variant="body2">Round: {gameDetails.round}</Typography>
+              )}
+              
+              </ListItem>
+            ))}
+          </List>
+        </Paper>
+      </div>
+    );
+  };
+
+}
